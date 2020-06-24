90e65cabe8600c937cc055aadaa1ab355454c418
qhauck16 <qhauck1@jhu.edu>
2020-06-24 17:24:04 -0400

    Trying LoRTIA annotations

---
+++ LoRTIA.sh
@@ -0,0 +1,2 @@
+~/software/LoRTIA/LoRTIA -5 TGCCATTAGGCCGGG --five_score 16 --check_in_soft 15 -3 AAAAAAAAAAAAAAA --three_score 16 -s poisson -f True /uru/Data/Nanopore/Analysis/gmoney/hbird/nanopore_rna/bam/nanopore_desalt2-1.5.4_XS/191021_m96.bam /uru/Data/Nanopore/Analysis/quinn/hbird/LoRTIA /uru/Data/Nanopore/Analysis/quinn/hbird/acolubris.fasta
+
